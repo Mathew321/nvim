9e95ef5de99afba636dca5ddcafbce0b80037f77
Mathew321 <mathew.fedasenka@gmail.com>
2024-06-11 18:22:42 +0200

    Commit fix

--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -5,3 +5,7 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
+
+-- Enable trail
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }

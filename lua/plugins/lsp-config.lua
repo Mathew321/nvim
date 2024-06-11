205fff6164eff1206d5216a20abe741c12578d85
Mathew321 <mathew.fedasenka@gmail.com>
2024-06-11 14:39:55 +0200

    updated neovim configuration

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,44 @@
+
+return {
+   {
+   "williamboman/mason.nvim",
+   config = function()
+      require("mason").setup()
+   end
+   },
+   {
+      "williamboman/mason-lspconfig.nvim",
+      config = function()
+         require("mason-lspconfig").setup({
+            ensure_installed = { "lua_ls", --Lua serv
+                                 "rust_analyzer", --Rust serv
+                                 "clangd", --C, Cpp serv
+                                 "jdtls", --Java serv
+                                 "quick_lint_js", --Javascript serv
+                                 "jedi_language_server", --Python serv
+            }
+         })
+      end
+   },
+   {
+      "neovim/nvim-lspconfig",
+      config = function()
+         local lspconfig = require("lspconfig")
+         local capabilities = require('cmp_nvim_lsp').default_capabilities()
+         lspconfig.lua_ls.setup({
+            capabilities = capabilities
+         })
+         lspconfig.jedi_language_server.setup({
+            capabilities = capabilities
+         })
+         lspconfig.jdtls.setup({
+            capabilities = capabilities
+         })
+
+         vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+         vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
+         vim.keymap.set( 'n', '<leader>ca', vim.lsp.buf.code_action, {})
+      end
+   }
+}
+

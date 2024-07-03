5b6d5e7bfa21e131ad065c559e9228e72cd30afa
Jack <jack@fedora.home>
2024-07-04 00:56:38 +0200

    Added Harpoon, and made few fixes with file management

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ if not vim.loop.fs_stat(lazypath) then
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
     lazypath,
-  })
+ })
 end
 vim.opt.rtp:prepend(lazypath)
 ---@diagnostic disable-next-line: unused-local
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,35 @@
-
 return {
    {
       'nvim-telescope/telescope.nvim', tag = '0.1.5',
       dependencies = { 'nvim-lua/plenary.nvim' },
       config = function()
          local builtin = require("telescope.builtin")
-         vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+         local actions = require("telescope.actions")
+         local action_state = require("telescope.actions.state")
+         local Path = require("plenary.path")
+         local harpoon_mark = require("harpoon.mark")
+         -- Custom grep function
+         local function grep_current_file()
+            local current_file = vim.fn.expand("%:p")
+            builtin.grep_string({
+               search = "",
+               search_dirs = { vim.fn.expand("~") },
+               attach_mappings = function(prompt_bufnr, map)
+                  actions.select_default:replace(function()
+                     local selection = action_state.get_selected_entry()
+                     actions.close(prompt_bufnr)
+                     harpoon_mark.add_file(selection.path)
+                     vim.cmd("edit " .. selection.path)
+                  end)
+                  return true
+               end,
+            })
+         end
+
+         -- Key mappings
+         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
          vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+         vim.keymap.set('n', '<leader>gf', grep_current_file, {}) -- New key mapping for grepping current file
       end
    },
    {
@@ -22,4 +45,15 @@ return {
          require("telescope").load_extension("ui-select")
       end
    },
+   {
+      'ThePrimeagen/harpoon',
+      config = function()
+         require("harpoon").setup()
+         vim.keymap.set('n', 'll', require("harpoon.mark").add_file, {})
+         vim.keymap.set('n', '<leader>tm', require("harpoon.ui").toggle_quick_menu, {})
+         vim.keymap.set('n', 'xx', require("harpoon.ui").nav_next, {})
+         vim.keymap.set('n', 'zz', require("harpoon.ui").nav_prev, {})
+      end
+   },
 }
+

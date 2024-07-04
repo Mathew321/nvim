02b466fe7992fb7a168babbfd3cbe97bbd16f314
Mathew321 <mathew.fedasenka@gmail.com>
2024-07-04 08:28:01 +0200

    Small change of for python code snippet keymap

--- lua/vim-keymaps.lua
+++ lua/vim-keymaps.lua
@@ -5,7 +5,7 @@ vim.keymap.set("n", '<leader>y', '"+yy')
 ---- Code snippets ----
 vim.keymap.set(
     "n",
-    "<leader>ee",
+    "<leader>ps",
     ":call append('.', ['class C():', '    def __init__(self):', '        pass', '', '    def start(self):', '        pass', '', '    def stop(self):', '        pass', '', 'def main():', '    pass', '', 'if __name__ == \"__main__\":', '    try:', '        main()', '    except KeyboardInterupt:', '        print(\"\\n\\n[INFO] System Exit!\")'])<CR>"
 )
 
